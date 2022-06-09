199e0b2eb7f93925b138fa5310e00bb9e96674c6
egrove <ellen.grove95@gmail.com>
2022-06-09 14:37:51 -0500

    built bubble chart code

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,67 @@
 jsonUrl = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
-function init() {
+// dropdown menu creation
+d3.json(jsonUrl).then(function (contents) {
+    // loop through subject IDs and create dropdown menu option for each
+    for (let i = 0; i < contents['names'].length; i++) {
+        d3.select("#selDataset").append('option')
+            .text(contents['names'][i])
+            .attr('value', contents['names'][i]);
+    }
+    });
+
+// initial charts
+function showSample() {
     d3.json(jsonUrl).then(function (contents) {
-        // store all subject IDs in an array
-        let subjectIds = contents['names'];
         
-        let dropdownMenu = d3.select("#selDataset");
+        let subjectId = d3.select("#selDataset").property('value');
 
-        // loop through subject IDs and create dropdown menu option for each
-        for (let i = 0; i < subjectIds.length; i++) {
-            dropdownMenu.append('option').text(subjectIds[i]);
-        };
-        console.log(subjectIds);
+        console.log(subjectId);
 
-        // let subjectId = dropdownMenu.text();
-        // console.log(subjectId);
-        let subjectId = '940';
+        // filter data for bar and bubble charts
         let subjectData = contents['samples'].filter((item) => item.id === subjectId);
 
         console.log(subjectData);
 
-        let x = subjectData[0]['sample_values'].slice(0,10).reverse();
-        let y = subjectData[0]['otu_ids'].map((item) => `OTU ${item}`).slice(0,10).reverse();
+        let barX = subjectData[0]['sample_values'].slice(0,10).reverse();
+        let barY = subjectData[0]['otu_ids'].map((item) => `OTU ${item}`).slice(0,10).reverse();
    
-        let data = [{
-            x : x,
-            y : y,
+        let barData = [{
+            x : barX,
+            y : barY,
             type : 'bar',
             orientation : 'h'
         }];
 
-        let layout = {
-            title : 'Test'
-        };
-        Plotly.newPlot("bar",data,layout);
+        Plotly.newPlot("bar",barData);
 
-        // filter metadata to retain info for relevant subject ID
+        let bubbleData = [{
+            x : subjectData[0]['otu_ids'],
+            y : subjectData[0]['sample_values'],
+            text : subjectData[0]['otu_labels'],
+            mode : 'markers',
+            marker: {
+                size : subjectData[0]['sample_values'],
+                color : subjectData[0]['otu_ids'],
+                colorscale : 'YlGnBu',
+                sizeref : 1.2
+            }
+        }];
+
+        let bubbleLayout = {
+            xaxis : {
+                title : {
+                    text : 'OTU ID'
+                }
+            }
+        };
+
+        Plotly.newPlot("bubble",bubbleData,bubbleLayout);
+
+
+        // filter metadata for demographic info
         let subjectMeta = contents['metadata'].filter((item) => item.id === parseInt(subjectId));
+        d3.selectAll('p').remove();
         // iterate through number of keys in object and append paragraph for each key-value pair
         for (let i = 0; i < Object.keys(subjectMeta[0]).length;i++) {
             d3.select('#sample-metadata')
@@ -47,7 +72,8 @@ function init() {
     });
 };
 
-init()
+showSample();
 
+d3.selectAll("#selDataset").on("change", showSample);
 
 
